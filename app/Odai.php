2c1bb4f712c08eca468acf916a87cc7eaa1cc916
Shuhei.Horiba <syu1010.soccer@gmail.com>
2018-07-05 04:13:04 +0000

    modified fillable

--- app/Odai.php
+++ app/Odai.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Odai extends Model
 {
+    protected $fillable = ['filename'];
+    
     function bokes()
     {
         return $this->hasMany(Boke::class);
